2bf2c0a1802edeea3f2f0f9c1e03e7bf3a4d8f7c
Surya Wisnugraha <85562110+plafound@users.noreply.github.com>
2024-01-23 20:57:32 +0700

    Update 2023_12_07_013157_create_log_users_table.php

--- database/migrations/2023_12_07_013157_create_log_users_table.php
+++ database/migrations/2023_12_07_013157_create_log_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->ulid('id')->primary();
             $table->foreignUlid('user_id');
             $table->string('activity');
+            $table->string('message')->nullable();
             $table->string('ip_address');
             $table->timestamps();
         });
